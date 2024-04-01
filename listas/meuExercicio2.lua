5ff81bbdf562140ca848d5cab181613d13f54dab
Lucasfranken131 <lucas.franco@aluno.fmpsc.edu.br>
2024-03-31 21:29:30 -0300

    feat: comparador de itens em lista

--- listas/meuExercicio2.lua
+++ listas/meuExercicio2.lua
@@ -1,13 +1,9 @@
-local Goblin = {
-    vida = 120,
-    ataque = 20,
-    defesa = 20
-}
-
-function Goblin:Dano(dano)
-    self.vida = self.vida - dano
+local array = {"pamonha", "cassarola", "torta", "tapioca"}
+local pedido = "cassarola"
+for i = 1, #array, 1 do
+    if pedido == array[i] then
+        print("O pedido ".. pedido.. " e igual ao pedido na posicao: ".. i)
+    else
+        print("O pedido ".. pedido.. " nao e igual ao pedido na posicao: ".. i)
+    end
 end
-
-Goblin:Dano(50)
-
-print(Goblin.vida)
