217206ee1d878e549c2172e03f79a67df54874f2
Lucasfranken131 <lucas.franco@aluno.fmpsc.edu.br>
2024-04-03 16:40:43 -0300

    feat: resolução exercício 23

--- decisao/exercicio23.lua
+++ decisao/exercicio23.lua
@@ -2,3 +2,21 @@
 alcançada por aluno e apresentar: A mensagem "Aprovado", se a média alcançada for maior ou igual a sete; 
 A mensagem "Reprovado", se a média for menor do que sete; A mensagem "Aprovado com Distinção", se a média for 
 igual a dez. --]]
+
+::inicio::
+io.write("A primeira nota: ")
+local nota1 = io.read()
+io.write("A segunda nota: ")
+local nota2 = io.read()
+local media = (nota1 + nota2) / 2
+print("A média do aluno: ".. media)
+if media == 10 then
+    print("Aprovado com Distinção.")
+elseif media >= 7 and media < 10 then
+    print("Aprovado.")
+elseif media > 10 then
+    print("Média inválida.")
+    goto inicio
+else
+    print("Reprovado")
+end
