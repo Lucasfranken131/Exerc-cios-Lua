42f093506131148fcbb0d5826e2467fbcba08570
Lucasfranken131 <lucas.franco@aluno.fmpsc.edu.br>
2024-07-31 09:50:46 -0300

    feat: correção busca binária

--- sorts/busca_binaria.lua
+++ sorts/busca_binaria.lua
@@ -1,19 +1,20 @@
+local array = { 2, 3, 5, 7, 11, 13, 17, 23, 29, 31, 37, 41, 47, 53}
 local min = 1
-local max = 20000
-local number = 299
+local max = #array
+local number = array[3]
 local media = math.floor((max + min) / 2);
 local correct = false;
 local tentativas = 0;
 while(correct == false) do
     tentativas = tentativas + 1;
     if(media == number) then
-        print("Acertou o numero!");
+        print("Acertou o numero: ".. number);
         print("Tentativas: ".. tentativas);
         correct = true;
     elseif(media > number) then
-        max = media;
+        max = media - 1;
     elseif(media < number) then
-        min = media;
+        min = media + 1;
     end
     media = math.floor((max + min) / 2);
 end
