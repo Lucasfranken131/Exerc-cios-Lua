0e1f3b39d366745ac98b58e7deb4f43325b612b0
Lucasfranken131 <lucas.franco@aluno.fmpsc.edu.br>
2024-07-31 14:10:20 -0300

    feat: correção da variável max

--- sorts/busca_binaria.lua
+++ sorts/busca_binaria.lua
@@ -1,11 +1,13 @@
-local array = { 2, 3, 5, 7, 11, 13, 17, 23, 29, 31, 37, 41, 47, 53}
+-- --local array = { 2, 3, 5, 7, 11, 13, 17, 23, 29, 31, 37, 41, 47, 53}
+local array = 100000000000
 local min = 1
-local max = #array
-local number = array[3]
+local max = array
+local number = 9999999832
 local media = math.floor((max + min) / 2);
 local correct = false;
 local tentativas = 0;
 while(correct == false) do
+    print(media)
     tentativas = tentativas + 1;
     if(media == number) then
         print("Acertou o numero: ".. number);
